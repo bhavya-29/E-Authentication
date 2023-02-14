39e416f5483664f991a197b041d4cc89663a3156
Munagala Venkata Naga LakshmiSaiBhavyaSri <70514092+bhavya-29@users.noreply.github.com>
2023-02-14 17:42:55 +0530

    Update app.py

--- login/app.py
+++ login/app.py
@@ -41,7 +41,7 @@ app.secret_key = 'your secret key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Workandchange@01'
+app.config['MYSQL_PASSWORD'] = '********'
 app.config['MYSQL_DB'] = 'authentication'
 
 mysql = MySQL(app)
